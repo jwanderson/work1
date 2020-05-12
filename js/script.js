992b44881abab647171c296358c60e23f9d69aa0
jwanderson <65142609+jwanderson@users.noreply.github.com>
2020-05-13 04:39:18 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(function(){
+  //.accordion_oneの中の.accordion_headerがクリックされたら
+  $('.s_01 .accordion_one .accordion_header').click(function(){
+    //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
+    $(this).next('.accordion_inner').slideToggle();
+    $(this).toggleClass("open");
+  });
+});
+
+$(function(){
+  var topBtn=$('#pageTop');
+  topBtn.hide();
+   
+  //◇ボタンの表示設定
+  $(window).scroll(function(){
+    if($(this).scrollTop()>80){
+      //---- 画面を80pxスクロールしたら、ボタンを表示する
+      topBtn.fadeIn();
+    }else{
+      //---- 画面が80pxより上なら、ボタンを表示しない
+      topBtn.fadeOut();
+    } 
+  });
+   
+  // ◇ボタンをクリックしたら、スクロールして上に戻る
+  topBtn.click(function(){
+    $('body,html').animate({
+    scrollTop: 0},500);
+    return false;
+  });
+  
+  });
